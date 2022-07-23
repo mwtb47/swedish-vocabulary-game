75156ca62f6537b863d9a27d6a18d495b81903c9
mwtb47 <57566091+mwtb47@users.noreply.github.com>
2022-07-23 10:57:03 +0200

    Add Telegram bot

---
+++ game/telegram/database.py
@@ -0,0 +1,53 @@
+"""Module with functions to fetch data from database.
+
+Functions:
+    fetch: Fetch the marks table from the database.
+    unix_to_datetime: Convert unix timestamp to a date.
+    count_answers: Count the number of answers during the
+        current day and week.
+"""
+
+from datetime import datetime
+import sqlite3
+
+import pandas as pd
+
+
+def fetch() -> pd.DataFrame:
+    """Fetch data from the marks table in the database.
+
+    Returns:
+        DataFrame with marks.
+    """
+    connection = sqlite3.connect("game/database/vocabulary.db")
+    df = pd.read_sql_query("SELECT * FROM betyg", connection)
+    connection.close()
+    return df
+
+
+def unix_to_date(timestamps: pd.Series) -> list:
+    """Convert unix timestamps to datetime date objects.
+
+    Args:
+        timestamps: Pandas series containing unix timestamps.
+
+    Returns:
+        Pandas series with datetime date objects.
+    """
+    return pd.to_datetime(timestamps, unit="s").apply(lambda x: x.date())
+
+
+def count_answers() -> tuple[int, int]:
+    """Count the number of answers on current day and current week.
+
+    Returns:
+        Tuple with the number of answers on current day and number
+        of answers during current week.
+    """
+    df = fetch()
+    today = datetime.now().date()
+    df["date"] = unix_to_date(df["tidsstämpel"])
+    df["week"] = [date.isocalendar().week for date in df.date]
+    day_count = len(df[df.date >= today])
+    week_count = len(df[df.week == today.isocalendar().week])
+    return day_count, week_count
---
+++ game/telegram/reminder.py
@@ -0,0 +1,62 @@
+"""Script to send a reminder if daily answer quota not reached.
+
+This script will send a message via a Telegram bot if fewer
+than 80 answers have been recorded during the current day. This
+script is run at 8pm each day using cron and the output is written
+to the log file telegram_bot.log.
+
+Fuctions:
+    send_message: Send a message via the Telegram bot.
+    check_status: Check the status of the send message request.
+    main: Send message if daily answer quota not reached.
+"""
+
+from datetime import datetime
+import requests
+
+from config import API_KEY, CHAT_ID
+from database import count_answers
+
+
+timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+def send_message(day: int, week: int) -> requests.Response:
+    """Send message from bot.
+
+    Args:
+        day: The number of answers today.
+        week: The number of answers in the current week.
+
+    Returns:
+        A response object.
+    """
+    text = f"You need to do more translations!\nDay: {day}/80\nWeek: {week}/560"
+    url = f"https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id={CHAT_ID}&text={text}"
+    return requests.get(url)
+
+
+def check_status(response: requests.Response) -> None:
+    """Check the status of the response.
+
+    Args:
+        response: Response object.
+    """
+    if response.status_code == 200:
+        print(f"{timestamp} - Message sent succesfully.")
+    else:
+        print(f"{timestamp} - Sending failed. Error {response.status_code}")
+
+
+def main() -> None:
+    """Main function to check answers and send reminder."""
+    day_count, week_count = count_answers()
+    if day_count < 80:
+        response = send_message(day_count, week_count)
+        check_status(response)
+    else:
+        print(f"{timestamp} - Target already achieved.")
+
+
+if __name__ == "__main__":
+    main()
